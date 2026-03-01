2913d08a02643e3311522cd7dad78673f536ea9f
coderynx <simone.filippi.1999@gmail.com>
2026-03-01 12:43:31 +0100

    fix: remove redundant include and simplify BCD-to-binary conversions

--- src/time.c
+++ src/time.c
@@ -1,6 +1,5 @@
 #include "time.h"
 #include "cmos.h"
-#include "io_ports.h"
 #include "types.h"
 
 // Get the current time and date from the CMOS
@@ -10,22 +9,22 @@ time_t get_time(void) {
 
   // Check if CMOS registers are using binary or BCD format
   uint8_t status = read_cmos(RTC_HOURS);
-  uint8_t binary = (status & CMOS_BINARY) ? 1 : 0;
+  uint8_t binary = status & CMOS_BINARY ? 1 : 0;
 
   // Read the seconds register
   time.seconds = read_cmos(RTC_SECONDS);
   if (!binary)
-    time.seconds = (time.seconds & 0x0F) + ((time.seconds / 16) * 10);
+    time.seconds = (time.seconds & 0x0F) + time.seconds / 16 * 10;
 
   // Read the minutes register
   time.minutes = read_cmos(RTC_MINUTES);
   if (!binary)
-    time.minutes = (time.minutes & 0x0F) + ((time.minutes / 16) * 10);
+    time.minutes = (time.minutes & 0x0F) + time.minutes / 16 * 10;
 
   // Read the hours register
   time.hours = read_cmos(RTC_HOURS);
   if (!binary)
-    time.hours = (time.hours & 0x0F) + ((time.hours / 16) * 10);
+    time.hours = (time.hours & 0x0F) + time.hours / 16 * 10;
 
   // Read the day of the week register
   time.day_of_week = read_cmos(RTC_DAY_OF_WEEK);
@@ -34,17 +33,17 @@ time_t get_time(void) {
   time.day_of_month = read_cmos(RTC_DAY_OF_MONTH);
   if (!binary)
     time.day_of_month =
-        (time.day_of_month & 0x0F) + ((time.day_of_month / 16) * 10);
+        (time.day_of_month & 0x0F) + time.day_of_month / 16 * 10;
 
   // Read the month register
   time.month = read_cmos(RTC_MONTH);
   if (!binary)
-    time.month = (time.month & 0x0F) + ((time.month / 16) * 10);
+    time.month = (time.month & 0x0F) + time.month / 16 * 10;
 
   // Read the year register
   time.year = read_cmos(RTC_YEAR);
   if (!binary)
-    time.year = (time.year & 0x0F) + ((time.year / 16) * 10);
+    time.year = (time.year & 0x0F) + time.year / 16 * 10;
 
   return time;
 }
